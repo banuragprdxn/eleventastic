563f7736926d57f9ff3d7a9bf1ba828a66fc1545
Anurag Banerjee <anurag.banerjee@prdxn.com>
2021-06-22 20:36:40 +0530

    feat: Add new SVGs, adding/update shortcodes, stylesheets
    - Add 2 custom SVGs
    - Updated with stylesheets
    - Updated existing shortcode (icon) to be passed without height and weight values (to be only managed by stylesheets)
    - Add a new shortcode (logo) with custom height and weight values
    - Add SVG (via sprite query) to index template

--- utils/shortcodes.js
+++ utils/shortcodes.js
@@ -1,6 +1,11 @@
 module.exports = {
     icon: function (name) {
-        return `<svg class="icon icon--${name}" role="img" aria-hidden="true" width="24" height="24">
+        return `<svg class="icon icon--${name}" role="img" aria-hidden="true">
+                    <use xlink:href="#icon-${name}"></use>
+                </svg>`
+    },
+    logo: function (name, width, height) {
+        return `<svg class="logo--${name}" role="img" aria-hidden="true" width="${width}", height="${height}">
                     <use xlink:href="#icon-${name}"></use>
                 </svg>`
     }
