605c983f276187ea80a4338596a7f303b9b17a21
Anurag Banerjee <me@anuragbanejee.com>
2021-06-23 12:59:35 +0530

    feat: Added filled shapes and updated the index page
    - Added new type which accepts fill color and paints the shape when returned
    - Updated index template to have labelled demos of some of the use cases

--- utils/shortcodes.js
+++ utils/shortcodes.js
@@ -5,8 +5,13 @@ module.exports = {
                 </svg>`
     },
     logo: function (name, width, height) {
-        return `<svg class="logo--${name}" role="img" aria-hidden="true" width="${width}", height="${height}">
+        return `<svg class="logo-default-${name}" role="img" aria-hidden="true" width="${width}", height="${height}">
                     <use xlink:href="#icon-${name}"></use>
+                </svg>`
+    },
+    colorlogo: function (name, width, height, fill) {
+        return `<svg class="logo-color-${name}" role="img" aria-hidden="true" width="${width}", height="${height}">
+                    <use xlink:href="#icon-${name}" fill="${fill}"></use>
                 </svg>`
     }
 }
